03cb3c96b14df7f4accbf537847b15f2db3bec62
Alberto <aaayza@gmail.com>
2022-05-25 17:04:32 -0300

    Style upgrades

--- src/components/CrudTable.js
+++ src/components/CrudTable.js
@@ -5,41 +5,39 @@ const CrudTable = ({ db, setDataToEdit, deleteData }) => {
   console.log(db);
   return (
     <>
-      <div id="table-div">
       <h2 id="users-title">Users:</h2>
-        <table className="table  table-striped table-bordered table-sm">
-          <thead>
-            <tr className="table-success" id="tr-table-header">
-              <th scope="col">Name</th>
-              <th scope="col">Username</th>
-              <th scope="col">Email</th>
-              <th scope="col">Street</th>
-              <th scope="col">Suite</th>
-              <th scope="col">City</th>
-              <th scope="col">Phone</th>
-              <th scope="col">Website</th>
-              <th scope="col">Company</th>
-              <th scope="col">Action</th>
+      <table className="table table-borderless table-success table-striped table-hover">
+        <thead>
+          <tr className="table-success" id="tr-table-header">
+            <th scope="col">Name</th>
+            <th scope="col">Username</th>
+            <th scope="col">Email</th>
+            <th scope="col">Street</th>
+            <th scope="col">Suite</th>
+            <th scope="col">City</th>
+            <th scope="col">Phone</th>
+            <th scope="col">Website</th>
+            <th scope="col">Company</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {!db.length ? (
+            <tr colSpan="10">
+              <td id="td-sin-datos">Sin Datos</td>
             </tr>
-          </thead>
-          <tbody>
-            {!db.length ? (
-              <tr colSpan="10">
-                <td id="td-sin-datos">Sin Datos</td>
-              </tr>
-            ) : (
-              db.map((e) => (
-                <CrudTableRow
-                  key={e.id}
-                  elem={e}
-                  setDataToEdit={setDataToEdit}
-                  deleteData={deleteData}
-                />
-              ))
-            )}
-          </tbody>
-        </table>
-      </div>
+          ) : (
+            db.map((e) => (
+              <CrudTableRow
+                key={e.id}
+                elem={e}
+                setDataToEdit={setDataToEdit}
+                deleteData={deleteData}
+              />
+            ))
+          )}
+        </tbody>
+      </table>
     </>
   );
 };
